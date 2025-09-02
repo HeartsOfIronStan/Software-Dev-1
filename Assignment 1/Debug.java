1f217f7e9ec8ff02659c4fba287fa754b9fbc8ab
HeartsOfIronStan <historian025@gmail.com>
2025-09-02 12:57:36 -0400

    Create Debug.java

---
+++ Assignment 1/Debug.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Debug {
+    public static void main(String[] args) {
+    int num1=10;
+    int num2=37;
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("How old are you? ");
+    String input = sc.nextLine();
+    System.out.println("You are " + input + " years old");
+
+            int sum = num1+num2;
+    System.out.println(sum);
+}}
+
+// I learned how to use Scanners and how to create an input in javascript.
